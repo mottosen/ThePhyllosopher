e37e8c876c9a32c5830c063afa18619de66567b0
Marius Ottosen <marius@appstract.co>
2024-12-24 00:50:41 +0100

    Category page working, some features TODO.

---
+++ ThePhyllosopherUmbraco/Utility/DateTimeUtility.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace ThePhyllosopherUmbraco.Utility
+{
+    public enum Month
+    {
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        September = 9,
+        October = 10,
+        November = 11,
+        December = 12
+    }
+
+    public class DateTimeUtility
+    {
+        static public string MonthDay(DateTime dateTime) => $"{(Month)dateTime.Month} {dateTime.Day}";
+        static public string MonthDayYear(DateTime dateTime) => $"{(Month)dateTime.Month} {dateTime.Day}, {dateTime.Year}";
+    }
+}
